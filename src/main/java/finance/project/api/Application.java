2e0d3dbd8500527a971ade2685cbc318cabfc0b1
ronanleflem <ronanleflem@gmail.com>
2025-11-08 20:37:30 +0100

    Active Async

--- src/main/java/finance/project/api/Application.java
+++ src/main/java/finance/project/api/Application.java
@@ -6,9 +6,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableAsync
 @EntityScan({"finance.project.api.entities", "finance.project.api.dataimport", "finance.project.api.live"})
 @EnableJpaRepositories({"finance.project.api.repositories", "finance.project.api.dataimport", "finance.project.api.live"})
 public class Application {
